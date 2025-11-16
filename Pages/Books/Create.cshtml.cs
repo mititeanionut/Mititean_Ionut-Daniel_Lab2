570c86463917b4306f0aab9b0726a0f2605e33eb
Ionuţ-Daniel Mititean <mititean.ionut@yahoo.ro>
2025-11-16 18:33:26 +0200

    Done

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -10,7 +10,7 @@ using Mititean_Ionut_Daniel_Lab2.Models;
 
 namespace Mititean_Ionut_Daniel_Lab2.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Mititean_Ionut_Daniel_Lab2.Data.Mititean_Ionut_Daniel_Lab2Context _context;
 
@@ -24,6 +24,9 @@ namespace Mititean_Ionut_Daniel_Lab2.Pages.Books
             // Key changed to "Author" to match the Razor view (asp-items="ViewBag.Author")
             ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "LastName");
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+            var book = new Book();
+            book.BookCategories = new List<BookCategory>();
+            PopulateAssignedCategoryData(_context, book);
             return Page();
         }
 
@@ -31,16 +34,24 @@ namespace Mititean_Ionut_Daniel_Lab2.Pages.Books
         public Book Book { get; set; } = default!;
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
         {
-            if (!ModelState.IsValid)
+            var newBook = new Book();
+            if (selectedCategories != null)
             {
-                return Page();
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
             }
-
+            Book.BookCategories = newBook.BookCategories;
             _context.Book.Add(Book);
             await _context.SaveChangesAsync();
-
             return RedirectToPage("./Index");
         }
     }
