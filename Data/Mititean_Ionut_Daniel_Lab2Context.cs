d99cf431076f43959921ecd25fa16664f286934f
Ionuţ-Daniel Mititean <mititean.ionut@yahoo.ro>
2025-11-16 18:05:01 +0200

    Added Pages/Categories

--- Data/Mititean_Ionut_Daniel_Lab2Context.cs
+++ Data/Mititean_Ionut_Daniel_Lab2Context.cs
@@ -17,5 +17,6 @@ namespace Mititean_Ionut_Daniel_Lab2.Data
         public DbSet<Mititean_Ionut_Daniel_Lab2.Models.Book> Book { get; set; } = default!;
         public DbSet<Mititean_Ionut_Daniel_Lab2.Models.Publisher> Publisher { get; set; } = default!;
         public DbSet<Mititean_Ionut_Daniel_Lab2.Models.Author> Author { get; set; } = default!;
+        public DbSet<Mititean_Ionut_Daniel_Lab2.Models.Category> Category { get; set; } = default!;
     }
 }
